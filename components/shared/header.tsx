6354fc72ec268ca835ed90325a0bbd54de418a48
eqinox <eqinox@abv.bg>
2026-01-14 15:24:34 +0200

    change about and revert header

--- components/shared/header.tsx
+++ components/shared/header.tsx
@@ -6,15 +6,12 @@ const Header = () => {
     <header className="h-auto lg:sticky lg:top-0 lg:flex lg:h-screen lg:max-h-screen lg:w-[48%] lg:flex-col lg:justify-between lg:py-24">
       <div className="z-40">
         <h1 className="text-4xl font-bold tracking-tight text-slate-200 sm:text-5xl">
-          EcoSense
+          Vitanoid
         </h1>
         <h2 className="mt-3 text-lg font-medium tracking-tight text-slate-200 sm:text-xl">
-          Предлагаме продукт Vitanoid
+          Филтър за пречистване на вода
         </h2>
 
-        <p className="mt-4 max-w-xs leading-normal">
-          Филтър за пречистване на вода
-        </p>
         <Nav />
       </div>
 
